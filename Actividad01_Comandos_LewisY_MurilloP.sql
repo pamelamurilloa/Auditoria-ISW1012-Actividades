647de56ec2b0dc471ff3a00a1528687f9f05031a
Pamela Murillo <pamelamurilloa@gmail.com>
2025-02-02 21:22:10 -0600

    Code for the justification of the fourth infraction has been added. Third has been skipped

--- Actividad01_Comandos_LewisY_MurilloP.sql
+++ Actividad01_Comandos_LewisY_MurilloP.sql
@@ -17,7 +17,7 @@ WITH
 			cnt.constraint_name,
 			cnt.constraint_type,
 			CASE COALESCE(cnt.constraint_name, '1')
-					WHEN '1' THEN 'No tiene una llave documentada'
+				WHEN '1' THEN 'No tiene una llave documentada'
 				END VALIDACION_PK
 		 FROM information_schema.tables tbl
 		 	LEFT JOIN table_constraint_pk cnt
@@ -30,7 +30,8 @@ WITH
 
 -- 02 - Productos sin Nombre.
 --		Regla de Integridad de Atributo - Restricción de Dominio.
-	WITH column_info AS (
+WITH
+	column_info AS (
 	    SELECT * 
 	    FROM information_schema.columns 
 	    WHERE table_schema = 'actividad01' 
@@ -51,8 +52,8 @@ WITH
 	    ON col.table_schema = tbl.table_schema
 	    AND col.table_name = tbl.table_name
 	WHERE tbl.table_schema = 'actividad01'
-	AND tbl.table_type NOT IN ('VIEW')
-	AND col.is_nullable IS NOT NULL
+		AND tbl.table_type NOT IN ('VIEW')
+		AND col.is_nullable IS NOT NULL
 	ORDER BY tbl.table_name;
 
 
@@ -60,8 +61,31 @@ WITH
 -- 		Regla de Integridad Referencial
 
 
--- 04 - Fechas Inconsistentes en nacimientos
---		Regla de Integridad de Dominio
+-- 04 - Fechas Inconsistentes de creación
+--		Regla de Integridad de Negocio
+	SELECT 
+	    tbl.table_schema AS ESQUEMA,
+	    tbl.table_name AS NOMBRE_DE_TABLA,
+	    tbl.table_type AS TIPO_DE_TABLA,
+		STRING_AGG(cli.fecha_creacion::TEXT, ', ') FILTER (WHERE cli.fecha_creacion > NOW()) AS FECHAS_EN_EL_FUTURO,
+	    CASE 
+	        WHEN COUNT(cli.fecha_creacion) FILTER (WHERE cli.fecha_creacion > NOW()) > 0 
+	        THEN 'Tiene fechas en el futuro'
+	        ELSE 'No tiene fechas en el futuro'
+	    END AS VALIDACION_FECHA_CREACION
+	FROM information_schema.tables tbl
+	LEFT JOIN information_schema.columns col
+	    ON col.table_schema = tbl.table_schema
+	    AND col.table_name = tbl.table_name
+	    AND col.column_name = 'fecha_creacion'
+	LEFT JOIN actividad01.cliente cli
+	    ON tbl.table_name = 'cliente'
+	    AND cli.fecha_creacion IS NOT NULL
+	WHERE tbl.table_schema = 'actividad01'
+		AND tbl.table_type NOT IN ('VIEW')
+		AND col.column_name IS NOT NULL
+	GROUP BY tbl.table_schema, tbl.table_name, tbl.table_type
+	ORDER BY tbl.table_name;
 
 
 -- 05 -
