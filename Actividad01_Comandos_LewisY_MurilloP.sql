76067bf31913cfee34ec2414a04bbf175561372f
Pamela Murillo <pamelamurilloa@gmail.com>
2025-02-02 20:26:35 -0600

    Add to repository the sql command file

---
+++ Actividad01_Comandos_LewisY_MurilloP.sql
@@ -0,0 +1,22 @@
+-- Actividad01 --
+-- Yeustin Lewis y Murillo Pamela
+
+-- Justificación de Hallazgos
+
+-- 01 - Falta de Llave Primaria Válida en Código de Producto.
+-- 		Regla de Integridad de Entidad - Llave Única.
+
+
+-- 02 - Productos sin Nombre.
+--		Regla de Integridad de Atributo - Restricción de Dominio.
+
+
+-- 03 - Llave Foránea no Detectada en Detalle de Pedidos hacia Pedidos.
+-- 		Regla de Integridad Referencial
+
+
+-- 04 - Fechas Inconsistentes en nacimientos
+--		Regla de Integridad de Dominio
+
+
+-- 05 - 
