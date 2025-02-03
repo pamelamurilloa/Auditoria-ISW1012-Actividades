a22e2681c0dbf19e2caf85cdcc64d64a6463287d
Pamela Murillo <pamelamurilloa@gmail.com>
2025-02-02 20:31:21 -0600

    Code for the justification of the first infraction has been added

--- Actividad01_Comandos_LewisY_MurilloP.sql
+++ Actividad01_Comandos_LewisY_MurilloP.sql
@@ -6,6 +6,21 @@
 -- 01 - Falta de Llave Primaria Válida en Código de Producto.
 -- 		Regla de Integridad de Entidad - Llave Única.
 
+WITH
+	table_constraint_pk AS (
+		SELECT *  FROM information_schema.table_constraints cnt
+		WHERE cnt.constraint_type = 'PRIMARY KEY')
+		SELECT tbl.table_schema ESQUEMA, tbl.table_name NOMBRE_DE_TABLA, tbl.table_type TIPO_DE_TABLA, 
+		cnt.constraint_name, cnt.constraint_type,
+		CASE COALESCE(cnt.constraint_name, '1') WHEN '1' THEN 'No tiene una llave documentada' END VALIDACION_PK
+		 FROM information_schema.tables tbl
+		 LEFT JOIN table_constraint_pk cnt
+		 ON cnt.table_schema = tbl.table_schema
+		 AND cnt.table_name = tbl.table_name
+		WHERE tbl.table_schema = 'actividad01'
+		 AND tbl.table_type NOT IN ('VIEW')
+		 AND cnt.table_name IS NULL
+		ORDER BY tbl.table_name;
 
 -- 02 - Productos sin Nombre.
 --		Regla de Integridad de Atributo - Restricción de Dominio.
@@ -19,4 +34,4 @@
 --		Regla de Integridad de Dominio
 
 
--- 05 - 
+-- 05 -
